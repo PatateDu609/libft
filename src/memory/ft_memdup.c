10f0b5681059dc78afd2b1fe7e6b58778f179bf8
PatateDu609 <gboucett@student.42.fr>
2022-06-24 14:42:27 +0200

    new libft

--- src/memory/ft_memdup.c
+++ src/memory/ft_memdup.c
@@ -4,7 +4,7 @@ uint8_t *ft_memdup(uint8_t *src, size_t len)
 {
 	uint8_t *dst;
 
-	if (!(dst = malloc(len)))
+	if (!(dst = malloc(len * sizeof *dst)))
 		return (NULL);
 	ft_memcpy(dst, src, len);
 	return (dst);
