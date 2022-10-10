37b3fa1e478f188e95744e922cbb24929f9c46fd
Ghali Boucetta <gboucett@student.42.fr>
2022-10-10 16:52:24 +0200

    Fixing ft_strndup

--- src/string/ft_strndup.c
+++ src/string/ft_strndup.c
@@ -8,8 +8,8 @@ char *ft_strndup(const char *s1, size_t n)
 	len = ft_strlen(s1);
 	if (n > len)
 		n = len;
-	if (!(ret = ft_calloc(n, sizeof *ret)))
+	if (!(ret = ft_calloc(n + 1, sizeof *ret)))
 		return (NULL);
-	ft_strlcpy(ret, s1, n);
+	ft_strlcpy(ret, s1, n + 1);
 	return (ret);
 }
